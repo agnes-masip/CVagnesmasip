5a303a12a4a19a68cbf69726d3fbf53dac9288aa
agnes.masip <agnes.masip@estudiantat.upc.edu>
2021-06-24 18:00:36 +0200

    Routing works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { StudiesComponent } from './components/studies/studies.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'studies', component: StudiesComponent},
+  {path:'projects', component: ProjectsComponent},
+  {path:'aboutMe', component: AboutMeComponent},
+  {path:'**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
+
+/////////////////// COMPONENTS ////////////////
+
 import { AppComponent } from './app.component';
-import { ContactInfoComponent } from './components/contact-info/contact-info.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { BackgroundDesignComponent } from './components/background-design/background-design.component';
-import { HomeComponent } from './components/home/home.component';
-import { StudiesComponent } from './components/studies/studies.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { AboutMeComponent } from './components/about-me/about-me.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactInfoComponent,
-    NavBarComponent,
-    BackgroundDesignComponent,
-    HomeComponent,
-    ProjectsComponent,
-    AboutMeComponent
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([
-      {path:'', component: HomeComponent},
-      {path:'/studies', component: StudiesComponent},
-      {path:'/projects', component: ProjectsComponent},
-      {path:'/aboutMe', component: HomeComponent}
-    ])
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
